9d98195a67620153fe85eb57ceeecc6a225bc761
Maksim Orlov <maksim.orlov@adviqo.com>
2019-12-20 13:23:48 +0100

    keycloak problem demo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,28 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+const React = require("react")
+const Keycloak = require("keycloak-js")
+const { KeycloakProvider } = require("@react-keycloak/web")
 
-// You can delete this file if you're not using it
+const keycloak = new Keycloak({
+  realm: process.env.REALM,
+  url: process.env.AUTH_URL,
+  clientId: process.env.AUTH_CLIENT_ID,
+})
+
+const Loading = () => <div>getting ready...</div>
+
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <KeycloakProvider
+      keycloak={keycloak}
+      initConfig={{
+        promiseType: "native",
+        onLoad: "check-sso",
+        silentCheckSsoRedirectUri:
+          window.location.origin + "/silent-check-sso.html",
+      }}
+      LoadingComponent={<Loading />}
+    >
+      {element}
+    </KeycloakProvider>
+  )
+}
